b4975584286d759c29ccb6f2c5e186771c6e7c82
franciscohb-ortiz <francisco.ortiz@negocios-digitales.com.mx>
2024-09-27 09:20:31 -0600

    Cadena para azure

--- config.js
+++ config.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const dbconnect = ()=>{
     mongoose.set('strictQuery', true);
     mongoose
-    .connect('mongodb+srv://test:test@cluster0.uaamn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    //COSMOS AZURE
+    .connect('mongodb+srv://test:123Tamarindo@test-soriana.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000')
+    //MONGO ATLAS
+    //.connect('mongodb+srv://test:test@cluster0.uaamn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .catch (error => console.log(error));
 }
 
